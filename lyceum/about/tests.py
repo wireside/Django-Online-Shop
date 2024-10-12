50bbe9ca8b16e0e5d5a2b0c86eb89e46c512130a
wireside <wiresifed@gmail.com>
2024-10-12 20:07:31 +0300

    Add tests

--- lyceum/about/tests.py
+++ lyceum/about/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+import unittest
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageURLTests(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_about_url(self):
+        url = reverse("about")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "О проекте")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,3 +1,24 @@
-# from django.test import TestCase
+import unittest
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageURLTests(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_item_list_url(self):
+        url = reverse("item_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Список элементов")
+
+    def test_item_detail_url(self):
+        url = reverse("item_detail", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Подробно элемент")
+
+
+if __name__ == "__main__":
+    unittest.main()
