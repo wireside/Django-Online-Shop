e0a2faa2f509f5146d4388b744cda2d028234a0b
Bogdan Salaev <wiresifed@gmail.com>
2024-10-13 22:55:28 +0300

    Fix

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.item_list, name="item_list"),
-    path("<int:item_id>", views.item_detail, name="item_detail"),
+    path("/", views.item_list, name="item_list"),
+    path("<int:item_id>/", views.item_detail, name="item_detail"),
 ]
