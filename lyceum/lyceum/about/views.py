72f60837584dff9b6cc1d731490e6bc486b50384
Богдан Салаев <bogdansalaeff@yandex.ru>
2024-11-04 11:06:43 +0000

    fix

--- lyceum/lyceum/about/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def description(request):
-    context = {}
-    return render(request, "about/about.html", context)
--- lyceum/lyceum/homepage/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomepageConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "homepage"
-    verbose_name = "Главная"
